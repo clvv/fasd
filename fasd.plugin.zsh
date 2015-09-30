919a8ce0b738dbcc75700702323bdd5638efdfe3
Travis Heller <trav.heller@gmail.com>
2015-09-30 01:30:56 -0700

    Fixed the nonexistent directory issues. . . twas a file, not a directory

--- fasd.plugin.zsh
+++ fasd.plugin.zsh
@@ -1,12 +1,12 @@
-FASD_DIR=/usr/local/bin/fasd
-if [ ! -d "$FASD_DIR" ]
+FASD=/usr/local/bin/fasd
+if [ ! -s "$FASD" ]
 then
 	#FASD_REPO=</path/to/fasd/>
-    echo $FASD_DIR does not exist
+    echo $FASD does not exist
     REPOS_DIR="$HOME/.antigen/repos"
     FASD_REPO="$REPOS_DIR/https-COLON--SLASH--SLASH-github.com-SLASH-allcatsarebeautiful-SLASH-fasd.git"
 	sudo make install -C $FASD_REPO
     source ~/.zshrc
 else
-    echo $FASD_DIR already exists
+    echo fasd is already installed! 
 fi
