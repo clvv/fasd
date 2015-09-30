0a246a7edc70b4c8d7b7e8c73cc8b8269d05e92f
Travis Heller <trav.heller@gmail.com>
2015-09-30 01:18:57 -0700

    Still need to fix the if directory does not exist condition

--- fasd.plugin.zsh
+++ fasd.plugin.zsh
@@ -1,6 +1,12 @@
-PREFIX=$HOME/.dotfiles/zsh/antigen/repos make install
-
-echo 'is this working?'
-printf '%s\n' "${PWD}"
-ls
-cat Makefile
+FASD_DIR=/usr/local/bin/fasd
+if [ ! -d "$FASD_DIR" ]
+then
+	#FASD_REPO=</path/to/fasd/>
+    echo $FASD_DIR does not exist
+    REPOS_DIR="$HOME/.antigen/repos"
+    FASD_REPO="$REPOS_DIR/https-COLON--SLASH--SLASH-github.com-SLASH-allcatsarebeautiful-SLASH-fasd.git"
+	sudo make install -C $FASD_REPO
+    source ~/.zshrc
+else
+    echo $FASD_DIR already exists
+fi
